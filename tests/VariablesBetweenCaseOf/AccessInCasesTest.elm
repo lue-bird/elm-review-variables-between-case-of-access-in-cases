2d051b7154cd2358336a2e66d017d9a822edfa44
lue-bird <lue.the.bird@gmail.com>
2023-07-07 16:25:00 +0200

    tests +

--- tests/VariablesBetweenCaseOf/AccessInCasesTest.elm
+++ tests/VariablesBetweenCaseOf/AccessInCasesTest.elm
@@ -45,4 +45,116 @@ For more details, see https://package.elm-lang.org/packages/lue-bird/elm-review-
                                 , end = { row = 7, column = 19 }
                                 }
                         ]
+        , test "should report an error when variable in tuple between `case .. of` is used in case" <|
+            \() ->
+                """module A exposing (..)
+a =
+    case ( list, () ) of
+        ( [], () ) ->
+            ( [], Nothing )
+        ( _ :: _, () ) ->
+            ( list, list |> List.minimum )
+"""
+                    |> Review.Test.run forbid
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message =
+                                "This variable in the case is used between `case .. of`"
+                            , details =
+                                [ "Use the information you matched in the case pattern instead of referring to the unmatched variable between `case .. of`."
+                                , """This can can prevent forgetting to use certain information and referring to the wrong variables.
+For more details, see https://package.elm-lang.org/packages/lue-bird/elm-review-variables-between-case-of-access-in-cases/latest#why"""
+                                ]
+                            , under = "list"
+                            }
+                            |> Review.Test.atExactly
+                                { start = { row = 7, column = 15 }
+                                , end = { row = 7, column = 19 }
+                                }
+                        ]
+        , test "should report an error when variable in list between `case .. of` is used in case" <|
+            \() ->
+                """module A exposing (..)
+a =
+    case [ list, [] ] of
+        [ [],[] ] ->
+            ( [], Nothing )
+        _ ->
+            ( list, list |> List.minimum )
+"""
+                    |> Review.Test.run forbid
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message =
+                                "This variable in the case is used between `case .. of`"
+                            , details =
+                                [ "Use the information you matched in the case pattern instead of referring to the unmatched variable between `case .. of`."
+                                , """This can can prevent forgetting to use certain information and referring to the wrong variables.
+For more details, see https://package.elm-lang.org/packages/lue-bird/elm-review-variables-between-case-of-access-in-cases/latest#why"""
+                                ]
+                            , under = "list"
+                            }
+                            |> Review.Test.atExactly
+                                { start = { row = 7, column = 15 }
+                                , end = { row = 7, column = 19 }
+                                }
+                        ]
+        , test "should report an error when variable in variant between `case .. of` is used in case" <|
+            \() ->
+                """module A exposing (..)
+a =
+    case Just list of
+        Just [] ->
+            ( [], Nothing )
+        _ ->
+            ( list, list |> List.minimum )
+"""
+                    |> Review.Test.run forbid
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message =
+                                "This variable in the case is used between `case .. of`"
+                            , details =
+                                [ "Use the information you matched in the case pattern instead of referring to the unmatched variable between `case .. of`."
+                                , """This can can prevent forgetting to use certain information and referring to the wrong variables.
+For more details, see https://package.elm-lang.org/packages/lue-bird/elm-review-variables-between-case-of-access-in-cases/latest#why"""
+                                ]
+                            , under = "list"
+                            }
+                            |> Review.Test.atExactly
+                                { start = { row = 7, column = 15 }
+                                , end = { row = 7, column = 19 }
+                                }
+                        ]
+        , test "should report an error when variable between `case .. of` is used in nested case" <|
+            \() ->
+                """module A exposing (..)
+a =
+    case Just list of
+        [] ->
+            ( [], Nothing )
+        _ :: tail ->
+            case tail of
+                [] ->
+                    ( [], Nothing )
+                _ :: _ ->
+                    ( list, list |> List.minimum )
+"""
+                    |> Review.Test.run forbid
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message =
+                                "This variable in the case is used between `case .. of`"
+                            , details =
+                                [ "Use the information you matched in the case pattern instead of referring to the unmatched variable between `case .. of`."
+                                , """This can can prevent forgetting to use certain information and referring to the wrong variables.
+For more details, see https://package.elm-lang.org/packages/lue-bird/elm-review-variables-between-case-of-access-in-cases/latest#why"""
+                                ]
+                            , under = "list"
+                            }
+                            |> Review.Test.atExactly
+                                { start = { row = 11, column = 23 }
+                                , end = { row = 11, column = 27 }
+                                }
+                        ]
         ]
